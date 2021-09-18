ec28e764e76416b18e96faf4b77ee6a3692b2af2
Adriel Isaiah Amoguis <adriel.isaiah.amoguis@gmail.com>
2021-09-18 13:27:14 +0800

    Finished Combination Field & Exponent Continuation

--- encode.js
+++ encode.js
@@ -5,18 +5,21 @@
 /**
  * This function will be called by the viewController when the user activates the encoder.
  * @param {*} decimalInput The given number in decimal format that will be converted to decimal 64 binary string.
- * @param {*} exponent This is for the exponent 
+ * @param {*} exponent This is for the exponent
  * @returns An object containing the steps taken and the final answer.
  */
-function encodeDecimal64(decimalInput) {
-    const steps = [];
-    let finalAnswer = '';
+function encodeDecimal64(decimalInput, exponent) {
+  // TODO: Implement this.
+  const { normalized, normalizedExponent } = normalize(decimalInput, exponent);
+  const signBit = getSign(decimalInput);
+  const ePrime = getEPrime(normalizedExponent);
+  const exponentBin = toBinPad(ePrime, 10);
+  const combinationField = getCF(normalized.charAt(0), exponentBin);
+  const exponentContinuation = String(exponentBin).substring(2, 11);
 
-    // TODO: Implement this.
-
-    // Returns the string array of steps taken that will be displayed to the user. This is expected to be a list of strings.
-    // Returns also the final answer -- this is expected to be of type String that contains the binary string.
-    return { steps, finalAnswer };
+  // Returns the string array of steps taken that will be displayed to the user. This is expected to be a list of strings.
+  // Returns also the final answer -- this is expected to be of type String that contains the binary string.
+  return `${signBit}${combinationField}${exponentContinuation}`;
 }
 
 /**
@@ -27,25 +30,28 @@ function encodeDecimal64(decimalInput) {
  * @returns A normalized version of the input. This will be in STRING format
  */
 function normalize(decimalInput, exponent) {
-
-  let normalized = ""
+  let normalized = "";
+  let exp = exponent;
 
   /** Test Case */
   //decimalInput = 1.234567;
 
   // Check first for decimal numbers
-  while (decimalInput % 1 != 0){
+  while (decimalInput % 1 != 0) {
     decimalInput *= 10;
-    exponent -= 1;
-  }
- 
-  // Get the number of digits from decimal input 
-  if (decimalInput.toString().length > 16 ) {
-    normalized = decimalInput.toString().padStart(16, "0");
+    exp -= 1;
   }
 
-  console.log(normalized);
-  console.log(exponent);
+  // Get the number of digits from decimal input
+  if (decimalInput.toString().length > 16) {
+    normalized = decimalInput.toString().padStart(16, "0");
+  } else {
+    normalized = decimalInput.toString();
+  }
+
+  //console.log("Normalized: ", normalized);
+  //console.log("Exponent: ", exp);
+  return { normalized, normalizedExponent: exp };
 }
 
 /**
@@ -56,10 +62,10 @@ function normalize(decimalInput, exponent) {
 function getSign(decimalInput) {
   let signBit = "";
 
-  if (decimalInput < 0) signBit="1";
-  else signBit="0";
+  if (decimalInput < 0) signBit = "1";
+  else signBit = "0";
 
-  console.log(signBit);
+  //console.log("Sign bit: ", signBit);
 
   return signBit;
 }
@@ -70,7 +76,8 @@ function getSign(decimalInput) {
  * @returns e+398 in INT format
  */
 function getEPrime(exponent) {
-  return exponent+398;
+  //console.log("ePrime: ", exponent + 398);
+  return exponent + 398;
 }
 
 /**
@@ -79,17 +86,45 @@ function getEPrime(exponent) {
  * @return number in binary format
  */
 function toBin(x) {
+  x = Number(x);
   return (x >>> 0).toString(2);
+}
+
+/**
+ * This function does the same thing as above, but pads to padLen digits in length.
+ * @param {*} digit
+ * @returns
+ */
+function toBinPad(digit, padLen) {
+  digit = Number(digit);
+  return (digit >>> 0).toString(2).padStart(padLen, 0);
 }
 
 /**
  * This function calls toBin() for first 2digits and getPrime() forlast 3 digits
  * @param  UNFINISHED
  */
-function getCF() {
-  let firstTwo = "";
-  let lastThree = "";
+function getCF(coefficientMSD, exponentBin) {
+  // Decide which implementation (0-7, 8-9)
+  const coeffMSD = Number(coefficientMSD);
+  const coeffMSDBin = toBinPad(coeffMSD, 4);
+  let exponentBits = "";
+  let coefficientBits = "";
+  if (coeffMSD < 8 && coeffMSD >= 0) {
+    // Implement: a b c d e
+    exponentBits = exponentBin.substring(0, 2);
+    coefficientBits = coeffMSDBin.substring(1, 4);
+  } else if (coeffMSD >= 8 && coeffMSD <= 9) {
+    // Implement: 1 1 c d e
+    exponentBits = "11" + exponentBin.substring(0, 2);
+    coefficientBits = coeffMSDBin.substring(3, 4);
+  }
 
-  firstTwo = toBin();
-  lastThree = getPrime();
+  const combination = (exponentBits + coefficientBits).padStart(5);
+
+  //   console.log("Exponent Field: ", exponentBits);
+  //   console.log("Coefficient Field: ", coefficientBits);
+  //   console.log("Combination Field: ", combination);
+
+  return combination;
 }
